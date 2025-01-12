251cd12e630413eeb8c226f7822b4e0931e5e3e4
Loic Majerus <59590960+Kinglo25@users.noreply.github.com>
2025-01-12 20:31:30 +0000

    next

--- python_decrypt.py
+++ python_decrypt.py
@@ -1,1 +1,7 @@
 
+ciphers = [  "0iq 4qmb56c 1iq ndzz raq0u 1jnosme hv iukswsa",
+            "0iq o90 jznntwq5 hv ut6 eqs4l bwn1akms mfc lpngk"]
+tab = "abcdefghijklmnopqrstuvwxyz0123456789"
+for cipher in ciphers:
+    print(cipher.replace("\t", ""))
+
